0372f769e512c32e4cfc27a5c02a5849439b4c4c
osoken <osoken.devel@outlook.jp>
2022-04-09 13:56:52 +0900

    feat(base): add MUI with emotion.

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "rgb(239, 182, 109)",
+        },
+        secondary: {
+            main: "rgb(107, 25, 64)",
+        },
+    }
+});
+
+export default theme;
