04172d511f84b552ef1958ccbab3ee6ebab8d145
osoken <osoken.devel@outlook.jp>
2022-04-09 14:13:53 +0900

    feat(main): change button tags to Button tags and arrange them with List component

--- components/main.jsx
+++ components/main.jsx
@@ -1,24 +1,53 @@
 import { AuthContext } from '../context/authContext';
 import { useContext } from 'react';
-
+import Button from '@mui/material/Button';
 import Link from 'next/link';
+import { css } from '@emotion/react';
 
 import styles from '../styles/main.module.css';
+import { Box, List, ListItem } from '@mui/material';
 
 export default function Main() {
     
     const { user } = useContext(AuthContext);
+    const menuButton = css`
+        width: 100%;
+    `;
 
     return (
         <div className={styles.homeContainer}>
             <h1>Welcome { user.displayName }!</h1>
-            <div className={styles.buttonPanel}>
-                <Link href="/record" passHref={true}><button>Record New Activity</button></Link>
-                <Link href="/input" passHref={true}><button>Input Routine</button></Link>
-                <Link href="/report" passHref={true}><button>View Report</button></Link>
-                <button>User Settings</button>
-            </div>
+            <Box>
+                <List>
+                    <ListItem>
+                        <Link href="/record" passHref={true} variant="contained">
+                            <Button variant="contained" color="secondary" css={menuButton}>
+                                Record New Activity
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Link href="/input" passHref={true}>
+                        <Button variant="contained" color="secondary" css={menuButton}>
+                                Input Routine
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Link href="/report" passHref={true}>
+                        <Button variant="contained" color="secondary" css={menuButton}>
+                                View Report
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                    <Button variant="contained" color="secondary" css={menuButton}>
+                            User Settings
+                        </Button>
+                    </ListItem>
+                </List>
+            </Box>
+            
         </div>
     );
-
 }
