4d32d4c91cded176e6d7b9da8a066a877c2590f9
Hiro Ogawa <hiro@rabbit-proto.com>
2022-04-16 17:58:53 +0900

    user AppBar in MUI

--- components/header.jsx
+++ components/header.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 
 import UserSettings from "./userSettings";
 
-import styles from 'styles/band.module.css'
-
-export default function AppHeader() {
+export default function ButtonAppBar() {
     const [state, setState] = React.useState({
         sidebar: false,
     });
@@ -25,26 +26,31 @@ export default function AppHeader() {
     };
 
     return (
-        <header className={styles.band}>
-        <div>
-            <React.Fragment key='sidebar'>
-                <Button onClick={toggleDrawer(true)}>➡️</Button>
-                <SwipeableDrawer
-                    open={state['sidebar']}
-                    onClose={toggleDrawer(false)}
-                    onOpen={toggleDrawer(true)}
-                >
-                    <Box
-                        sx={{ width: 250 }}
-                        role="presentation"
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" centerTitle="true">
+            <Toolbar>
+                <React.Fragment key='sidebar'>
+                    <Button color="inherit" onClick={toggleDrawer(true)}>Menu</Button>
+                    <SwipeableDrawer
+                        open={state['sidebar']}
+                        onClose={toggleDrawer(false)}
+                        onOpen={toggleDrawer(true)}
                     >
-                        <Button onClick={toggleDrawer(false)}>⬅</Button>
-                        <UserSettings />
-                    </Box>
-                </SwipeableDrawer>
-            </React.Fragment>
-        </div>
-        <h1>家事分担</h1>
-        </header>
+                        <Box
+                            sx={{ width: 250 }}
+                            role="presentation"
+                        >
+                            <Button color="inherit" onClick={toggleDrawer(false)}>Close</Button>
+                            <UserSettings />
+                        </Box>
+                    </SwipeableDrawer>
+                </React.Fragment>
+                <Typography variant="h4" component="div" sx={{ flexGrow: 1 }} align="center">
+                    家事分担
+                </Typography>
+                {/* <Button color="inherit">Logout</Button> */}
+            </Toolbar>
+        </AppBar>
+        </Box>
     );
 }
