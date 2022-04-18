ffb58d436dbdaf1f0a1e5ff31ff648ac29dc325b
osoken <osoken.devel@outlook.jp>
2022-04-18 20:37:23 +0900

    feat: add constants.js

---
+++ src/constants.js
@@ -0,0 +1,34 @@
+const allTasks = [
+    {
+        name: "料理",
+        children: [
+            {name: '朝ご飯', checked: false},
+            {name: '弁当', checked: false},
+            {name: '昼ご飯', checked: true},
+            {name: '夕ご飯', checked: true},
+            {name: '買い物', checked: true}
+        ],
+    }, {
+        name: "掃除",
+        children: [
+            {name: 'リビング', checked: false},
+            {name: 'お風呂場', checked: true},
+            {name: 'トイレ', checked: false}
+        ],
+    }, {
+        name: "子育て",
+        children: [
+            {name: '音読', checked: false},
+            {name: '宿題', checked: false},
+            {name: 'お迎え', checked: false},
+            {name: '病院連れ', checked: false},
+        ]
+    }, {
+        name: "その他",
+        children: [
+            {name: '皿洗い', checked: true},
+        ]
+    }
+];
+
+export default { allTasks };
