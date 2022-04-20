ec09940cc37cdb7a05c72958673e6a7b057d463b
osoken <osoken.devel@outlook.jp>
2022-04-20 19:13:41 +0900

    fix: fix wrong data

--- components/resultDashboard.jsx
+++ components/resultDashboard.jsx
@@ -2,7 +2,6 @@ import ScatterPlotComponent from "./scatterPlotComponent";
 import { Box } from "@mui/material";
 
 export default function ResultDashboard(props) {
-  
   return (
     <Box>
       <ScatterPlotComponent value={props.value}></ScatterPlotComponent>
--- components/scatterPlot.jsx
+++ components/scatterPlot.jsx
@@ -4,13 +4,16 @@ import * as d3 from "d3";
 export default function ScatterPlot(props) {
   const ref = useRef();
   const margin = 30;
+  const jitter = 10;
   const graphWidth = props.width - 2 * margin;
   const graphHeight = props.height - 2 * margin;
   const xScale = d3.scalePoint([0, graphWidth]).domain([-1, 0, 1])
   const yScale = d3.scaleLinear([graphHeight, 0]).domain([0, 120])
+  function genJitter() {
+    return jitter * (Math.random() - 0.5);
+  }
   useEffect(() => {
     const svg = d3.select(ref.current);
-    console.log(svg);
     svg.select("#xAxis").call(d3.axisBottom().scale(xScale));
     svg.select("#yAxis").call(d3.axisLeft().scale(yScale));
   });
@@ -20,10 +23,10 @@ export default function ScatterPlot(props) {
         <g id="xAxis" transform={`translate(0, ${graphHeight})`}></g>
         <g id="yAxis"></g>
         {
-          Object.keys(props.value.myTasks).filter(d => d.participates).map(d => {return <circle cx={xScale(d.effort)} cy={yScale(d.duration)} fill="#FF0000" r={10}></circle>})
+          Object.keys(props.value.myTasks).map(k => props.value.myTasks[k]).filter(d => d.participates).map(d => {return <circle key={Math.random()} cx={xScale(d.effort) + genJitter()} cy={yScale(d.duration) + genJitter()} fill={props.myColor} r={10}></circle>})
         }
         {
-          Object.keys(props.value.partnerTasks).filter(d => d.participates).map(d => {return <circle cx={xScale(d.effort)} cy={yScale(d.duration)} fill="#0000FF" r={10}></circle>})
+          Object.keys(props.value.partnerTasks).map(k => props.value.partnerTasks[k]).filter(d => d.participates).map(d => {return <circle key={Math.random()} cx={xScale(d.effort) + genJitter()} cy={yScale(d.duration) + genJitter()} fill={props.partnerColor} r={10}></circle>})
         }
       </g>
     </svg>
