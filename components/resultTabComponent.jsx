d54990796565b8ca28a4bfa69f8e6b8c09c7db45
osoken <osoken.devel@outlook.jp>
2022-04-23 17:12:55 +0900

    feat: divide result screen into tabs

--- components/resultTabComponent.jsx
+++ components/resultTabComponent.jsx
@@ -2,6 +2,7 @@ import { TabContext, TabPanel, TabList } from "@mui/lab";
 import { Tab } from "@mui/material";
 import { useState } from "react";
 import ResultDashboard from "./resultDashboard";
+import AllocationList from "./allocationList";
 
 export default function ResultTabComponent(props) {
   const [tabNum, setTabNum] = useState("1");
@@ -16,18 +17,18 @@ export default function ResultTabComponent(props) {
         <Tab label="分担提案2" value="3" />
       </TabList>
       <TabPanel value="1" sx={{ width: 1}}>
-        私<ul>{<li>{props.currentAliceAllocation}</li>}</ul>
-        パートナー<ul>{<li>{props.currentBobAllocation}</li>}</ul>
+        <AllocationList head="私" data={props.currentAliceAllocation}></AllocationList>
+        <AllocationList head="パートナー" data={props.currentBobAllocation}></AllocationList>
         <ResultDashboard value={ props.currentTaskRepartition }></ResultDashboard>
       </TabPanel>
       <TabPanel value="2" sx={{ width: 1}}>
-        私<ul>{<li>{props.adjustedWinnerAliceAllocation}</li>}</ul>
-        パートナー<ul>{<li>{props.adjustedWinnerBobAllocation}</li>}</ul>
+      <AllocationList head="私" data={props.adjustedWinnerAliceAllocation}></AllocationList>
+        <AllocationList head="パートナー" data={props.adjustedWinnerBobAllocation}></AllocationList>
         <ResultDashboard value={ props.adjustedWinnerTaskRepartition }></ResultDashboard>
       </TabPanel>
       <TabPanel value="3" sx={{ width: 1}}>
-        私<ul>{<li>{props.leastChangeAliceAllocation}</li>}</ul>
-        パートナー<ul>{<li>{props.leastChangeBobAllocation}</li>}</ul>
+      <AllocationList head="私" data={props.leastChangeAliceAllocation}></AllocationList>
+        <AllocationList head="パートナー" data={props.leastChangeBobAllocation}></AllocationList>
         <ResultDashboard value={ props.leastChangeAllocationTaskRepartition }></ResultDashboard>
       </TabPanel>
     </TabContext>
