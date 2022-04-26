8f7dad03c8bb09a254fc8080014f170271d250cb
osoken <osoken.devel@outlook.jp>
2022-04-26 23:03:03 +0900

    feat(result): add table component

---
+++ components/resultTableComponent.jsx
@@ -0,0 +1,28 @@
+import { Grid } from "@mui/material";
+import SingleResultTable from "./singleResultTable";
+
+export default function ResultTableComponent(props) {
+  console.log(props);
+  const myTasks = Object.keys(props.value.myTasks).map(k => {return {
+    name: k,
+    participates: props.value.myTasks[k].participates,
+    effort: props.value.myTasks[k].effort,
+    duration: props.value.myTasks[k].duration,
+  }});
+  const partnerTasks = Object.keys(props.value.partnerTasks).map(k => {return {
+    name: k,
+    participates: props.value.partnerTasks[k].participates,
+    effort: props.value.partnerTasks[k].effort,
+    duration: props.value.partnerTasks[k].duration,
+  }});
+  return (
+    <Grid container spacing={2} sx={{alignItems: "center", borderBottom: 1, borderColor: "divider", width: "100%"}}>
+      <Grid item xs={6}>
+        <SingleResultTable name="私" data={myTasks}></SingleResultTable>
+      </Grid>
+      <Grid item xs={6}>
+        <SingleResultTable name="パートナー" data={partnerTasks}></SingleResultTable>
+      </Grid>
+    </Grid>
+  );
+}
