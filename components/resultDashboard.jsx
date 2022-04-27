5daedaf59572756291b0408060744515cccb04a3
Yokoyama <41771358+tomonatu8@users.noreply.github.com>
2022-04-27 18:58:44 +0900

    Revert "feat(result): add table component"

--- components/resultDashboard.jsx
+++ components/resultDashboard.jsx
@@ -1,11 +1,9 @@
 import ScatterPlotComponent from "./scatterPlotComponent";
-import ResultTableComponent from "./resultTableComponent";
 import { Box } from "@mui/material";
 
 export default function ResultDashboard(props) {
   return (
     <Box>
-      <ResultTableComponent value={props.value}></ResultTableComponent>
       <ScatterPlotComponent value={props.value}></ScatterPlotComponent>
     </Box>
   )
