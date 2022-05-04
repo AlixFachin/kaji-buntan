977ec8bcc7a5ea2b0180de6f5e25db6690a2b339
yokoyama <yokoyama@miraishare.jp>
2022-05-04 17:04:24 +0900

    change graph

--- components/landingPage.jsx
+++ components/landingPage.jsx
@@ -1,10 +1,10 @@
 // Simple Landing Page component which will contain just enough controls to log in
 // This page is used as a fallback when users are not logged
 
-import Image from 'next/image';
+//import Image from 'next/image';
 import AuthDialog from 'components/auth/authDialog';
 import { useState } from 'react';
-import mainImage from 'public/undraw_positive_attitude_re_wu7d.svg';
+//import mainImage from 'public/undraw_positive_attitude_re_wu7d.svg';
 import Link from 'next/link';
 
 
@@ -23,7 +23,7 @@ export default function LandingPage() {
         <main>
                 { isAuthDialogVisible ? <AuthDialog hideDialog={ hideAuthDialog }/> : '' }
                 <h1>家事分担コンセルジュ</h1>
-                <Image src={mainImage} alt="couple discussing"  />
+                {/* <Image src={mainImage} alt="couple discussing"  /> */}
                 <p> 平等の家事分担の計算見ましょう！</p>
                 <Link href="/input" passHref={true} ><button>次へ</button></Link>
         </main>
