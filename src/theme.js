4ca75c21b35ecace783f6250184616ddaaab76e7
yokoyama <yokoyama@miraishare.jp>
2022-05-10 03:32:17 +0900

    色を紫に

--- src/theme.js
+++ src/theme.js
@@ -3,15 +3,15 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
     palette: {
         primary: {
-            main: "#A5559C",
+            main: "#9c27b0",
         },
         secondary: {
-            main: "#A5559C",
+            main: "#9c27b0",
         },
     },
     typography: {
         h4: {
-            backgroundColor: "#A5559C",
+            backgroundColor: "#9c27b0",
             paddingLeft: '1em',
             paddingRight: '1em',
             borderRadius: '15px',
